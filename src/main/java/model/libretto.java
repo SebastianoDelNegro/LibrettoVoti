b8a7fa06ae93565aa96d006c5dfe0389b446ff61
sebas <sebas@LAPTOP-KAHT28ES.fritz.box>
2021-03-09 11:23:48 +0100

    prime modifiche

--- src/main/java/model/libretto.java
+++ src/main/java/model/libretto.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.*;
+
+/**
+ * gestisce un insieme di esami superati
+ * @author sebas
+ *
+ */
+
 public class libretto {
+	private List<voto> voti;
+	/**
+	 * creo un libretto nuovo, vuoto
+	 */
+	public libretto() {
+		this.voti = new ArrayList<voto>();
+	}
+	
+	
 
 }
--- src/main/java/model/voto.java
+++ src/main/java/model/voto.java
@@ -1,5 +1,56 @@
 package model;
 
+import java.time.LocalDate;
+
+/**
+ * classe voto contiene informazioni su un esame superato
+ * @author sebas
+ *
+ */
 public class voto {
+	private String corso;
+	private int voto;
+	private LocalDate data;
+	
+	/**
+	 * costruisceun nuovo voto.
+	 * @param corso nome del corso superato
+	 * @param voto valore del voto acquisito
+	 * @param data di superamento esame
+	 */
+	
+	public voto(String corso, int voto, LocalDate data) {
+		this.corso = corso;
+		this.voto = voto;
+		this.data = data;
+	}
+
+	public String getCorso() {
+		return corso;
+	}
+
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+
+	public int getVoto() {
+		return voto;
+	}
+
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+	
+	
 
 }
